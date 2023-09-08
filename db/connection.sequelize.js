ba2729a3fe56a5e10b0efa10c467e93a7288803f
Anis KCHAOU <16862158+aniskchaou@users.noreply.github.com>
2023-09-08 17:54:20 +0200

    Update connection.sequelize.js

--- db/connection.sequelize.js
+++ db/connection.sequelize.js
@@ -1,4 +1,4 @@
-
+/*
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
@@ -14,7 +14,7 @@ module.exports = {
         idle: 10000
     }
 };
-
+*/
 /*
 module.exports = {
     HOST: "ec2-3-223-242-224.compute-1.amazonaws.com",
@@ -31,3 +31,18 @@ module.exports = {
         idle: 10000
     }
 };*/
+module.exports = {
+    HOST: "ec2-52-19-55-12.eu-west-1.compute.amazonaws.com",
+    USER: "aaiouigqdmvxmd",
+    PASSWORD: "e9df61f11102547d973d738e59c0f9df741ed9100d0c08c08b89073d24eb9a38",
+    DB: "d2m86v4lv51lge",
+    dialect: "postgres",
+    port: 5432,
+    native: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
